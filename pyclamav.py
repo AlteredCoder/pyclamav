ad2dea852ed9200b29c7a736fddb42f13500502b
Kevin KADOSH <kevin@crowdsec.net>
2025-01-02 17:16:27 +0100

    up

--- pyclamav.py
+++ pyclamav.py
@@ -5,14 +5,18 @@ from lib.log import get_logger
 
 from lib.scan import Scan
 
+
 def main():
     config = load_config()
     logger = get_logger(config.log_folder, config.verbose)
-    scanner = Scan(config.nb_process, config.modified_file_datetime,logger)
+    scanner = Scan(config.nb_process, config.modified_file_datetime, logger)
 
     logger.info(f"Scanning with {config.nb_process} process")
     for folder in config.folders:
-        logger.info(f"Scanning folder for last {config.modified_file_duration} changes", extra={"folder": folder})
+        logger.info(
+            f"Scanning folder for last {config.modified_file_duration} changes",
+            extra={"folder": folder},
+        )
         r = scanner.scan_folder(folder)
 
 
