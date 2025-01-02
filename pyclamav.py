629f6345e67456e65fffd98b807619004535d35a
Kevin KADOSH <kevin@crowdsec.net>
2025-01-02 21:45:10 +0100

    Fix Scan class init

--- pyclamav.py
+++ pyclamav.py
@@ -9,12 +9,12 @@ from lib.scan import Scan
 def main():
     config = load_config()
     logger = get_logger(config.log_folder, config.verbose)
-    scanner = Scan(config.nb_process, config.modified_file_datetime, logger)
+    scanner = Scan(config.modified_file_datetime, logger)
 
-    logger.info(f"Scanning with {config.nb_process} process")
+    logger.info(f"Scanning {len(config.folders)} folders with files changed during the last {config.modified_file_since}")
     for folder in config.folders:
         logger.info(
-            f"Scanning folder for last {config.modified_file_duration} changes",
+            f"Scanning folder",
             extra={"folder": folder},
         )
         r = scanner.scan_folder(folder)
